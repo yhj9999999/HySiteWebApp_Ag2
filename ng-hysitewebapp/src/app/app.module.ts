954687b466f7ddbaab267b8c2712d4cff0496868
haojie yu <yhj9999999@hotmail.com>
2019-06-10 00:43:10 -0400

    basic setup

--- ng-hysitewebapp/src/app/app.module.ts
+++ ng-hysitewebapp/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        NgProgressModule.withConfig({ spinner: false, color: 'red' }),
+        NgProgressHttpModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
